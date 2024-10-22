e55b061b1815931ce906a19efbcab6efa5198844
hvxxn <162563757+hvxxn@users.noreply.github.com>
2024-10-22 11:00:20 +0900

    중간고사

---
+++ code/1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE student
+( Shreya CHAR(10) NOT NULL,
+Rianna CHAR(10) NOT NULL,
+Yosef CHAR(10) NOT NULL,
+);
+
+CREATE TABLE student_course
+( 1 int(1) NOT NULL,
+1 int(1) NOT NULL,
+2 int(1) NOT NULL,
+2 int(1) NOT NULL,
+2 int(1) NOT NULL,
+3 int(1) NOT NULL,
+);
+
+CREATE TABLE course
+( Database design CHAR(20) NOT NULL,
+English literature CHAR(20) NOT NULL,
+Python programming CHAR(20) NOT NULL,
+);
+
+CREATE TABLE teacher
+( Taylah CHAR(10) NOT NULL,
+Sarah-Louise CHAR(10) NOT NULL,
+);
